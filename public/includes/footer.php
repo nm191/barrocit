175617d8527ba3f9f831eb6283f03e38994b1f4b
Nick van Meel <nickvanmeel95@gmail.com>
2016-10-10 12:37:27 +0200

    Footer Fix

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,13 +1,9 @@
 </div>
-<<<<<<< HEAD
+
 <div class="footer flex">
     <span>Copyright &copy; 2016 Barroc IT</span>
     <span class="text-right"><img src="../public/assets/img/logo.png" alt="ROTINI LOGO"></span>
-=======
-<div class="footer">
-    <span class="copyright">Copyright &copy; 2016 Barroc IT</span>
->>>>>>> origin/master
-</div>
 
+</div>
 </body>
 </html>
